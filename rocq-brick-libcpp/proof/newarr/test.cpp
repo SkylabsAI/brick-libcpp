257eddc0fdd300c0fdb442d5861959e20d05ef8d
Abhishek Anand (Monad Labs) <abhishek.anand.iitg@gmail.com>
2025-10-27 22:12:38 -0400

    test specs and proofs. delete proof has issues

---
+++ rocq-brick-libcpp/proof/newarr/test.cpp
@@ -0,0 +1,25 @@
+#include<memory>
+
+int * testnew() {
+  int *x;
+  x=new int;
+  *x=1;
+  return x;
+}
+
+int * testnewarr() {
+  int *x=new int[2];
+  x[0]=1;
+  x[1]=2;
+  return x;
+}
+
+void testnewdel() {
+  int *x = testnew();
+  delete x;
+}
+
+void testnewarrdel() {
+  int *x = testnewarr();
+  delete x;
+}
