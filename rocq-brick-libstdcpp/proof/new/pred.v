1af4d0f0451a3c3c3cd1a1508b4051c0e1b8daf2
Gregory Malecha <gregory@skylabs-ai.com>
2025-10-30 23:23:08 -0400

    Cleanup.

--- rocq-brick-libstdcpp/proof/new/pred.v
+++ rocq-brick-libstdcpp/proof/new/pred.v
@@ -23,14 +23,21 @@ Section with_cpp.
 
 End with_cpp.
 
+(* TODO: upstream this! *)
 Class IsNotArray (ty : type) : Prop :=
   { _ : if ty is Tarray _ _ then False else True }.
 #[global] Hint Extern 0 (IsNotArray _) => (constructor; exact I) : typeclass_instances.
 
+(* TODO: upstream this! *)
+(** This guarantees that non-array allocations have 0 overhead bytes. *)
+#[global] Declare Instance new_token_non_array_0_overhead : forall `{Σ : cpp_logic} {σ : genv} ty q storage_p overhead,
+    IsNotArray ty ->
+    Observe [| overhead = 0%N |] (new_token.R q $ new_token.mk ty storage_p overhead).
+
 NES.Begin alloc.
 
   (** [token ty sz q] bundles the dynamic allocation tokens provided by
-      the C++ abstract machine.
+      the C++ abstract machine with the [allocatedR] fact from the allocation library.
    *)
   br.lock
   Definition token `{Σ : cpp_logic} {σ : genv} (ty : type) (q : Qp) : Rep :=
@@ -38,11 +45,5 @@ NES.Begin alloc.
       [| size_of _ ty = Some sz |] **
       new_token.R q {| new_token.alloc_ty := ty ; new_token.storage_ptr := storage_p ; new_token.overhead := overhead |} **
       pureR (storage_p .[ "unsigned char" ! -overhead ] |-> allocatedR q (overhead + sz)).
-
-  (* TODO: upstream this! *)
-  (** This guarantees that non-array allocations have 0 overhead bytes. *)
-  #[global] Declare Instance new_token_non_array_0_overhead : forall `{Σ : cpp_logic} {σ : genv} ty q storage_p overhead,
-      IsNotArray ty ->
-    Observe [| overhead = 0%N |] (new_token.R q $ new_token.mk ty storage_p overhead).
 
 NES.End alloc.
