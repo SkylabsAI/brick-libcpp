6e92b6655060cdd005e7db0faa2bdc59bfc1acd8
rinshankaihou <madoka@umich.edu>
2025-11-04 12:50:01 -0600

    refactor recursive mutex proof; need to fix transfer_ok

--- rocq-brick-libstdcpp/proof/mutex/demo_cpp_proof.v
+++ rocq-brick-libstdcpp/proof/mutex/demo_cpp_proof.v
@@ -23,53 +23,19 @@ Section with_cpp.
     as_Rep (fun this : ptr =>
       recursive_mutex.inv_rmutex γ (∃ a_b : tele_arg _, tele_app (P this) a_b)).
 
-  Definition update {TT : tele} (f : TT -t> TT)
-    (x : recursive_mutex.acquire_state TT)
-    : recursive_mutex.acquire_state TT :=
-    match x with
-    | recursive_mutex.NotHeld => recursive_mutex.NotHeld
-    | recursive_mutex.Held n xs => recursive_mutex.Held n (tele_app f xs)
-    end.
-
   cpp.spec "C::update_a(int)" with
     (\this this
      \arg{x} "x" (Vint x)
      \prepost{γ q} this |-> CR γ q
      \pre{args th} recursive_mutex.acquireable γ th args (TT:=TT) (P this)
-     \post recursive_mutex.acquireable γ th (TT:=TT) (update (TT:=TT) (fun (a b : Z) => mk (a+x) b) args) (P this)).
+     \post recursive_mutex.acquireable γ th (TT:=TT) (recursive_mutex.update (TT:=TT) (fun (a b : Z) => mk (a+x) b) args) (P this)).
 
   cpp.spec "C::update_b(int)" with
     (\this this
      \arg{x} "x" (Vint x)
      \prepost{γ q} this |-> CR γ q
      \pre{args th} recursive_mutex.acquireable γ th args (TT:=TT) (P this)
-     \post recursive_mutex.acquireable γ th (TT:=TT) (update (TT:=TT) (fun (a b : Z) => mk a (b + x)) args) (P this)).
-
-  #[global] Instance acquireable_learn γ th TT : LearnEq2 (@recursive_mutex.acquireable _ _ _ γ th TT).
-  Proof. solve_learnable. Qed.
-
-  (* TODO make this into a hint *)
-  Lemma is_held {t1 t2: recursive_mutex.acquire_state TT} :
-    recursive_mutex.acquire t1 t2 ->
-    ∃ n xs, t2 = recursive_mutex.Held n xs /\
-      t1 = recursive_mutex.release t2.
-  Proof.
-    intros.
-    destruct t1; simpl in H; eauto.
-    - exists 0. naive_solver.
-    - exists (S n). naive_solver.
-  Qed.
-
-  #[program]
-  Definition acquireable_is_acquired_C {TT : tele} g th t t' P
-      (_ : recursive_mutex.acquire (TT:=TT) t t') :=
-    \cancelx
-    \consuming recursive_mutex.acquireable g th t' P
-    \deduce{n args} tele_app P args
-    \deduce [| t' = recursive_mutex.Held n args /\ match n with 0 => t = recursive_mutex.NotHeld 
-      | S n' => t = recursive_mutex.Held n' args end |]
-    \end.
-  Next Obligation. Admitted.
+     \post recursive_mutex.acquireable γ th (TT:=TT) (recursive_mutex.update (TT:=TT) (fun (a b : Z) => mk a (b + x)) args) (P this)).
 
   Lemma update_a_ok : verify[source] "C::update_a(int)".
   Proof.
@@ -78,16 +44,16 @@ Section with_cpp.
     iExists _; iExists TT; iExists (P this); iExists q; iExists th.
     go. ego.
     (* we know t must be in a held state *)
-    destruct (is_held H) as (n & xs & -> & ?).
-    simpl in *. subst.
-    rewrite /P/=. destruct xs as [a [b []]]; simpl.
+    rewrite /P/=.
+    destruct args as [a [b []]]; simpl.
     go.
-    iSplitR. { admit. }
+    iSplitR. { admit. (* TODO make the addition modulo arithmetic in the spec *) }
     go.
     iExists γ; iExists TT; iExists (P this); iExists q; iExists th; iExists n; iExists (mk (_ + x) _).
     go. rewrite /P. go.
     rewrite /P. go.
-    destruct n; by subst.
+    erewrite recursive_mutex.update_eq; last done.
+    go.
   Admitted.
 
   cpp.spec "C::transfer(int)" with
@@ -95,9 +61,8 @@ Section with_cpp.
       \arg{x} "x" (Vint x)
       \prepost{γ q} this |-> CR γ q
       \pre{args th} recursive_mutex.acquireable γ th args (TT:=TT) (P this)
-      \post recursive_mutex.acquireable γ th (TT:=TT) (update (TT:=TT) (fun (a b : Z) => mk (a+x) (b-x)) args) (P this)).
+      \post recursive_mutex.acquireable γ th (TT:=TT) (recursive_mutex.update (TT:=TT) (fun (a b : Z) => mk (a+x) (b-x)) args) (P this)).
 
-  Opaque recursive_mutex.acquireable.
   Lemma transfer_ok : verify[source] "C::transfer(int)".
   Proof.
     verify_spec; go.
@@ -105,8 +70,28 @@ Section with_cpp.
     iExists γ; iExists TT; iExists (P this); iExists q; iExists th; iExists _. go.
     { ego. }
     iExists γ; iExists q; iExists _; iExists th; go.
-    { ego. }
     rewrite /CR.
+
+    Import spec.recursive_mutex.
+    Set Nested Proofs Allowed.
+      #[program]
+    Definition own_P_is_acquireable_C {TT : tele} g th n P args :=
+      \cancelx
+      \consuming tele_app P args
+      \consuming own g (◯E (S n))
+      \deduce acquireable(TT:=TT) g th (Held n args) P
+      \end.
+    Next Obligation.
+      intros.
+      rewrite /acquireable.
+      go.
+    Qed.
+    
+    Hint Resolve own_P_is_acquireable_C : br_hints.
+
+    (* FIXME *)
+    go.
+
     iExists γ; iExists q; iExists _; iExists th; go.
     iSplitL; [ | admit ].
     go.
