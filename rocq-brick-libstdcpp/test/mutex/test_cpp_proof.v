1770be11be7dfefb533fd4575e3cda40fa267980
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-09-04 13:46:17 +0200

    WIP on porting mutex client

--- rocq-brick-libstdcpp/test/mutex/test_cpp_proof.v
+++ rocq-brick-libstdcpp/test/mutex/test_cpp_proof.v
@@ -8,12 +8,48 @@ Section with_cpp.
   Context `{Σ : cpp_logic, σ : genv} {HAS_THREADS : HasStdThreads Σ}.
   Context `{!lockG Σ}.
 
-  cpp.spec "test_mutex()" as test_mutex_spec from source with (\post emp).
+  cpp.spec "test_mutex()" as test_mutex_spec from source with (
+        \post emp).
+
+  (* #[program] *)
+  (* Definition login_C E (th : thread_idT) (g : gname) (s : gset thread_idT) := *)
+  (*   \cancelx *)
+  (*   \masks MatchFUpd E E => NoConfig *)
+  (*   \consuming lock_ghost.used_threads g s *)
+  (*   \preserving current_thread th *)
+  (*   (* \require th ∉ s *) *)
+  (*   \using [| th ∉ s |] *)
+  (*   \deduce lock_ghost.users g {[th]} *)
+  (*   \deduce lock_ghost.used_threads g (s ∪ {[th]}) *)
+  (*   \end. *)
+  (* Next Obligation. *)
+  (*   intros; work. *)
+  (*   wapply (lock_ghost.login th g s); first set_solver; work. *)
+  (*   iModIntro; work. *)
+  (* Qed. *)
+
+  Import wp_notations.Verbose.
 
   Theorem test_mutex_ok : verify[source] "test_mutex()".
   Proof.
     verify_spec; go.
-    iExists emp. go.
+    iExists emp.
+    wuntil Emember_call go.
+
+    ren_hyp g gname.
+    wapply current_thread_always_exists; work.
+    wapply (lock_ghost.login t g ∅); first set_solver.
+
+    (* Depends on fewer details, but more awkward. *)
+    Succeed
+      solve [
+        wuntil (FreeTemps.delete "std::mutex") go; run1; iApply fupd_wp_destroy_named;
+        wapply (lock_ghost.logout t g ∅); first set_solver; work; iModIntro; go].
+
+    (* Easier, but more fragile: [Kfree] might disappear. *)
+    wuntil Kfree go.
+    wapply (lock_ghost.logout t g ∅); first set_solver.
+    go.
   Qed.
 
   cpp.spec "test_lock_guard()" as test_lock_guard_spec from source with (\post emp).
@@ -21,7 +57,17 @@ Section with_cpp.
   Lemma test_lock_guard_ok : verify[source] "test_lock_guard()".
   Proof.
     verify_spec; go.
-    iExists emp; go.
+    iExists emp.
+    wuntil Econstructor go.
+
+    ren_hyp g gname.
+    wapply current_thread_always_exists; work.
+    wapply (lock_ghost.login t g ∅); first set_solver.
+    wuntil Kfree go; run1; wuntil Kfree go.
+
+    wapply (lock_ghost.logout t g ∅); first set_solver.
+
+    go.
   Qed.
 
   cpp.spec "test_scoped_lock()" as test_scoped_lock_spec from source with (\post emp).
@@ -30,18 +76,35 @@ Section with_cpp.
   Proof.
     verify_spec; go.
     iExists emp; go.
-    iExists emp; go.
+    iExists emp.
+    wuntil Econstructor go.
+    wapply current_thread_always_exists; work.
+    wapply (lock_ghost.login t _ ∅); last work with br_erefl; first set_solver.
+    wapply (lock_ghost.login t _ ∅); last work with br_erefl; first set_solver.
+    wuntil Kfree go. run1.
+    wuntil interp go.
+    wapply (lock_ghost.logout t _ ∅); first set_solver.
+    step with br_erefl.
+    wapply (lock_ghost.logout t _ ∅); first set_solver.
+    go with br_erefl.
   Qed.
 
   cpp.spec "test_unique_lock()" as test_unique_lock_spec from source with (\post emp).
 
   Lemma test_unique_lock_ok : verify[source] "test_unique_lock()".
   Proof.
-    verify_spec; go.
+    verify_spec.
+    go.
+    iExists emp.
+    wuntil Econstructor go.
 
-    iExists emp; go.
+    ren_hyp g gname.
+    wapply current_thread_always_exists; work.
+    wapply (lock_ghost.login t g ∅); first set_solver.
+    wuntil Kfree go; run1; wuntil Kfree go.
+    wapply (lock_ghost.logout t g ∅); first set_solver.
+    go.
   Qed.
-
 
   cpp.spec "test_unique_lock_defer()" as test_unique_lock_defer_spec from source with (
     \prepost{q} _global "std::defer_lock" |-> defer_lock_t.R q
@@ -60,7 +123,15 @@ Section with_cpp.
 
   Lemma test_unique_lock_move_ok : verify[source] "test_unique_lock_move()".
   Proof.
-    verify_spec; go.
-    iExists emp; go.
+    verify_spec; go. iExists emp.
+    wuntil Econstructor go.
+
+    ren_hyp g gname.
+    wapply current_thread_always_exists; work.
+    wapply (lock_ghost.login t g ∅); first set_solver.
+
+    wuntil Kfree go.
+    wapply (lock_ghost.logout t g ∅); first set_solver.
+    go.
   Qed.
 End with_cpp.
